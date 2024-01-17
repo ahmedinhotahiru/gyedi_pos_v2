4a5fd5c071e718d6cd342680b8e17b085088aa2a
ahmedinhotahiru <issahahmed00@gmail.com>
2024-01-17 15:52:52 +0200

    final version 2

--- admin/dbh/db.php
+++ admin/dbh/db.php
@@ -1,9 +1,14 @@
 <?php
 
-define('SERVER_NAME', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'gyedi_enterprise');
+// define('SERVER_NAME', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'gyedi_enterprise');
+
+define('SERVER_NAME', 'sdb-54.hosting.stackcp.net');
+define('DB_USER', 'gyedi_user');
+define('DB_PASSWORD', ':O@sCTrqmDyA');
+define('DB_NAME', 'gyedi_enterprise-35303036fd88');
 
 $db = mysqli_connect(SERVER_NAME, DB_USER, DB_PASSWORD, DB_NAME);
 
